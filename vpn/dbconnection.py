2fa62bf899ea42f4036934d6f6f666af382de21c
Marcus <97861729+MarcusCWD@users.noreply.github.com>
2024-01-22 13:35:12 +0800

    updated env

--- vpn/dbconnection.py
+++ vpn/dbconnection.py
@@ -1,15 +1,20 @@
 import os
 from sqlalchemy import create_engine
+from urllib.parse import quote
 
-# Connection to Database i.e 'mysql+mysqlconnector://root@localhost:3307/market.db'
+# Connection to Database i.e 'mysql+mysqlconnector://root:password@localhost:3307/market.db'
 database_connector = 'mysql+mysqlconnector'
-DATABASE_URL = (database_connector + '://'
-                + os.environ.get('DATABASE_USER')
-                + '@'
-                + os.environ.get('DATABASE_ADDRESS')
-                + ':'
-                + os.environ.get('DATABASE_PORT')
-                + '/'
-                + os.environ.get('DATABASE_NAME'))
+DATABASE_URL = (
+    f'{database_connector}://'
+    + os.environ.get('DATABASE_USER')
+    + ':'
+    + quote(os.environ.get('DATABASE_PASSWORD'))
+    + '@'
+    + os.environ.get('DATABASE_ADDRESS')
+    + ':'
+    + os.environ.get('DATABASE_PORT')
+    + '/'
+    + os.environ.get('DATABASE_NAME')
+)
 
 engine = create_engine(DATABASE_URL, echo=True)
