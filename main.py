7123da94adf3d16cc1d9434dff2a44f686e2fe50
Marcus <97861729+MarcusCWD@users.noreply.github.com>
2024-01-24 13:44:30 +0800

    updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from vpn import app
 import os
 
 if __name__ == '__main__':
-    # app.run(debug=True, port=os.environ.get('APP_PORT'))
+    host=os.environ.get('APP_HOST')
+    port = os.environ.get('APP_PORT')
+    print(f'App Starting on host {host} with port {port}')
     from waitress import serve
-    serve(app, host=os.environ.get('APP_HOST'), port=os.environ.get('APP_PORT'))
+    serve(app, host=host, port=port)
 
