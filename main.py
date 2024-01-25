fc093b2180e8248785e83bd66cabc4a602913ab7
Marcus <97861729+MarcusCWD@users.noreply.github.com>
2024-01-25 11:09:02 +0800

    updated init

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from vpn import app
 import os
 
 if __name__ == '__main__':
-    host=os.environ.get('APP_HOST')
+    host = os.environ.get('APP_HOST')
     port = os.environ.get('APP_PORT')
-    print(f'App Starting on host {host} with port {port}')
+
+    print(f'App started using host {host} on port {port}')
+
     from waitress import serve
     serve(app, host=host, port=port)
 
--- vpn/__init__.py
+++ vpn/__init__.py
@@ -20,8 +20,13 @@ bcrypt = Bcrypt(app)
 # SECRET_KEY is used for CSRF
 app.config['SECRET_KEY'] = os.environ.get('CSRF_SECRET_KEY')
 
-# Create the database tables
-# Base.metadata.create_all(bind=engine)
+# In development mode only will tables be made automatically,
+# if no pre-existing ones are existing
+if os.environ.get('DEVELOPER_MODE') == 'True':
+    print('Developer mode on')
+
+    # Create the database tables
+    Base.metadata.create_all(bind=engine)
 
 # Create a SQLAlchemy session
 db = Session(bind=engine)
